be0a2b29741bacefb64742066d5c625ce34f2006
Bazlur Rashid <mailtobazlur@gmail.com>
2025-11-05 23:31:54 +0600

    - vehicle edit updated

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'red-hero': '#E63946',
+        'blue-hero': '#457B9D',
+        'yellow-hero': '#F4A261',
+        'soft': '#FAFAFA',
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
